3ccfecb7195ebfef9d1cb9fef23a650e048843b1
Git-Cat-21 <khushmish21@gmail.com>
2025-09-29 14:06:54 +0530

    locking file works

--- locker.sh
+++ locker.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/bash
 
 if [[ "$1" == "-lock" || "$1" == "-l" ]]; then 
-    echo "Enter your password here "
-    read -s password
-    echo "Enter your password again "
-    read -s confirm_pass
+    read -p "enter the file name you wish to encrypt: "  file 
+    ls | grep "$file"
+    if [[ $? -eq 0 ]]; then 
+        echo "File exists"
 
-    if [[ "$password" == "$confirm_pass" ]]; then
-        read -p "enter the file name you wish to encrypt: "  file 
+        echo "Enter your password here "
+        read -s password
+        echo "Enter your password again "
+        read -s confirm_pass
 
-        ls | grep "$file"
-        if [[ $? -eq 0 ]]; then 
-            echo "File exists"
+        if [[ "$password" == "$confirm_pass" ]]; then
+            echo "Yayy you entered the password correctly"
+            openssl enc -aes-256-cbc -pbkdf2 -salt \
+            -pass pass:"$password" \
+            -in "$file" -out "$file".enc
+        
+            rm "$file"
+
         else
-            echo "No such file"
+            echo "Wrong password inputted"
         fi
-
-        echo "Yayy you entered the password correctly"
     else
-        echo "Wrong password inputted"
+        echo "No such file"
     fi
 fi
