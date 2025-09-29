2aa0cfec30f612c1cde76699d43c0c6f5eedfef7
Git-Cat-21 <khushmish21@gmail.com>
2025-09-29 15:26:22 +0530

    all works

---
+++ README.md
@@ -0,0 +1,19 @@
+# File Locker Script
+
+## A simple bash script to encrypt and decrypt files using OpenSSL with AES-256-CBC encryption.
+
+### Usage
+```bash
+git clone https://github.com/Git-Cat-21/Locker
+cd Locker
+chmod +x locker.sh
+sudo cp ./locker.sh /usr/local/bin/locker
+```
+
+### To view help
+```bash
+locker
+```
+
+## Warning
+Always remember your password! If you forget it, there is no way to recover your encrypted files.
--- locker.sh
+++ locker.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/bash
 
+usage() {
+    clear
+    echo -e "\033[1;35m╔══════════════════════════════════════╗\033[0m"
+    echo -e "\033[1;35m║         \033[1;36m🔒 FILE LOCKER TOOL🔒        \033[1;35m║\033[0m"
+    echo -e "\033[1;35m╚══════════════════════════════════════╝\033[0m"
+    echo
+    echo -e "\033[1;33mDescription:\033[0m"
+    echo -e "  \033[1;37mA simple utility to encrypt and decrypt files using OpenSSL\033[0m"
+    echo
+    echo -e "\033[1;36mUsage:\033[0m \033[1;37mlocker.sh [OPTION]\033[0m"
+    echo
+    echo -e "\033[1;32mOptions:\033[0m"
+    echo -e "  \033[1;34m-lock, -l\033[0m      \033[1;37mEncrypt a file with password protection\033[0m"
+    echo -e "  \033[1;34m-unlock, -u\033[0m    \033[1;37mDecrypt a previously encrypted file\033[0m"
+}
+
 if [[ "$1" == "-lock" || "$1" == "-l" ]]; then 
     read -p "enter the file name you wish to encrypt: "  file 
+
     ls | grep "$file"
     if [[ $? -eq 0 ]]; then 
         echo "File exists"
@@ -18,18 +35,18 @@ if [[ "$1" == "-lock" || "$1" == "-l" ]]; then
             -in "$file" -out "$file".enc
         
             rm "$file"
-
         else
             echo "Wrong password inputted"
         fi
+
     else
         echo "No such file"
     fi
 
 elif [[ "$1" == '-unlock' || "$1" == "-u" ]]; then
     read -p "enter the file name you wish to decrypt without .enc: "  file 
-    ls | grep "$file"
 
+    ls | grep "$file"
     if [[ "$?" -eq 0 ]]; then 
         echo "Enter the password" 
         read -s password
@@ -48,5 +65,11 @@ elif [[ "$1" == '-unlock' || "$1" == "-u" ]]; then
         echo "File doesnt exist"
     
     fi
+
+elif [[ -z "$1" ]]; then 
+    usage
+
+else
+    echo "wrong argument"
 
 fi
