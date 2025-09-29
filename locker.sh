1a72577c33f7996b1321e4e02ae1c8616a97ad93
Git-Cat-21 <khushmish21@gmail.com>
2025-09-29 15:55:14 +0530

    refactored

--- locker.sh
+++ locker.sh
@@ -19,57 +19,63 @@ usage() {
 if [[ "$1" == "-lock" || "$1" == "-l" ]]; then 
     read -p "enter the file name you wish to encrypt: "  file 
 
-    ls | grep "$file"
-    if [[ $? -eq 0 ]]; then 
-        echo "File exists"
+    if [[ ! -f "$file" ]]; then
+        echo "No such file"
+        exit 1
+    fi
+    echo "File exists"
 
-        echo "Enter your password here "
-        read -s password
-        echo "Enter your password again "
-        read -s confirm_pass
+    read -s -p "Enter your password here: " password
+    echo     
+    read -s -p "Enter your password again: " confirm_pass
+    echo
 
-        if [[ "$password" == "$confirm_pass" ]]; then
-            echo "Yayy you entered the password correctly"
-            openssl enc -aes-256-cbc -pbkdf2 -salt \
-            -pass pass:"$password" \
-            -in "$file" -out "$file".enc
+    if [[ "$password" != "$confirm_pass" ]]; then
+        echo "Passwords do not match"
+        exit 1
+    fi
+    echo 
+
+    echo "Encrypting File....."
+    if openssl enc -aes-256-cbc -pbkdf2 -salt \
+        -pass pass:"$password" \
+        -in "$file" -out "$file".enc; then
         
-            rm "$file"
-        else
-            echo "Wrong password inputted"
-        fi
+        rm -f "$file"
+        echo "File encrypted successfully: $file.enc"
 
     else
-        echo "No such file"
+        echo "Encryption failed"
     fi
 
 elif [[ "$1" == '-unlock' || "$1" == "-u" ]]; then
     read -p "enter the file name you wish to decrypt without .enc: "  file 
 
-    ls | grep "$file"
-    if [[ "$?" -eq 0 ]]; then 
-        echo "Enter the password" 
-        read -s password
-        openssl enc -d -aes-256-cbc -pbkdf2 \
-        -pass pass:"$password" \
-        -in "$file".enc -out "$file"
+    if [[ ! -f "$file.enc" ]]; then 
+        echo "No such file"
+        exit 1
+    fi
+    
+        echo 
+        read -s -p "Enter the password: " password
+        echo
 
-        if [[ "$?" -eq 0 ]]; then 
-            echo "File unlocked"
-            rm "$file".enc
+        echo "Decrypting file...."
+        if openssl enc -d -aes-256-cbc -pbkdf2 \
+            -pass pass:"$password" \
+            -in "$file".enc -out "$file"; then 
+
+            rm -f "$file".enc
+            echo "File decrypted successfully"
         else
+            rm -f "$file"
             echo "Wrong Password"
         fi
 
-    else
-        echo "File doesnt exist"
-    
-    fi
-
-elif [[ -z "$1" ]]; then 
+elif [[ -z "$1" || "$1" == '-h' ]]; then 
     usage
 
 else
-    echo "wrong argument"
+    echo "Invalid option."
 
 fi
